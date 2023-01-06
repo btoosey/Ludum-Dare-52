6411a827c6cc6eb976f21308a80f6ce4af11ab33
Ben Toosey <41492238+btoosey@users.noreply.github.com>
2023-01-06 21:29:25 +0000

    ADD: Player and movement
    Movement is based on a grid, and only allows horizontal and vertical movement

---
+++ Ludum Dare 52/Assets/Scripts/GridManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField] private int width, height;
+
+    void GenerateGrid()
+	{
+		for (int x = 0; x < width; x++)
+		{
+			for (int y = 0; y < height; y++)
+			{
+				//GenerateGrid
+			}
+		}
+	}
+}
