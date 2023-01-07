ca77c7379e90f8ec57dbd925c246155889abe92c
Ben Toosey <41492238+btoosey@users.noreply.github.com>
2023-01-07 09:04:58 +0000

    ADD: Dangers
    These dangers simply destroy the player on collision

--- Ludum Dare 52/Assets/Scripts/PlayerMovement.cs
+++ Ludum Dare 52/Assets/Scripts/PlayerMovement.cs
@@ -2,9 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-
     public float moveSpeed = 5f;
     public Transform movePoint;
 
@@ -18,24 +17,26 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
-        if (Vector3.Distance(transform.position, movePoint.position)  <= .05f)
-		{
+        if (Vector3.Distance(transform.position, movePoint.position) <= .05f)
+        {
             if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
-		    {
-                if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, whatStopsMovement))
-				{
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, whatStopsMovement))
+                {
                     movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
-				}
-		    } else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+                }
+            }
+            else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
             {
                 if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .2f, whatStopsMovement))
-				{
+                {
                     movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
-				}
+                }
             }
-		}
+        }
 
-        
+
     }
 }
